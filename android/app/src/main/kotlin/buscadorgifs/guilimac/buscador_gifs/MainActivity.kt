8575c80c493ac52bd9c61c655e30d7fd99f3a459
Guilherme de Lima Conceição <guilherme.lima@ed.company>
2020-12-22 22:55:55 -0300

    estrutura de buscado de gifs do giphy retornando dados

---
+++ android/app/src/main/kotlin/buscadorgifs/guilimac/buscador_gifs/MainActivity.kt
@@ -0,0 +1,6 @@
+package buscadorgifs.guilimac.buscador_gifs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/home_page.dart
@@ -0,0 +1,104 @@
+import "package:flutter/material.dart";
+import 'package:http/http.dart' as http;
+import "dart:convert";
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String _search;
+
+  int _offset = 0;
+
+  Future<Map> _getGifs() async {
+    http.Response response;
+    if (_search == null)
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/trending?api_key=2vLyrSgP9193JypPWXBwMe0nuPn4CtL7&limit=20&rating=g");
+    else
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/search?api_key=2vLyrSgP9193JypPWXBwMe0nuPn4CtL7&q=$_search&limit=20&offset=$_offset&rating=g&lang=en");
+    return json.decode(response.body);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _getGifs().then((map) {
+      print(map);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Image.network(
+            "https://developers.giphy.com/static/img/dev-logo-lg.7404c00322a8.gif"),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: "Pesquise aqui",
+                labelStyle: TextStyle(color: Colors.white),
+                border: OutlineInputBorder(),
+              ),
+              style: TextStyle(color: Colors.white, fontSize: 18),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Expanded(
+              child: FutureBuilder(
+                future: _getGifs(),
+                builder: (context,snapshot){
+                  switch(snapshot.connectionState){
+                    case ConnectionState.none:
+                    case ConnectionState.waiting:
+                      return Container(
+                        width: 200,
+                        height: 200,
+                        alignment: Alignment.center,
+                        child: CircularProgressIndicator(
+                          valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                          strokeWidth: 5,
+                        ),
+                      );
+                    default:
+                      if(snapshot.hasError) return Container();
+                      return _createGifTable(context, snapshot);
+                  }
+                },
+              )
+          ),
+        ],
+      ),
+    );
+  }
+  Widget _createGifTable(context, snapshot){
+    return GridView.builder(
+        padding: EdgeInsets.all(10),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        itemCount: snapshot.data["data"].length,
+        itemBuilder: (context,index){
+          return GestureDetector(
+            child: Image.network(snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+            height: 300,
+            fit: BoxFit.cover,),
+          );
+        }
+    );
+  }
+}
